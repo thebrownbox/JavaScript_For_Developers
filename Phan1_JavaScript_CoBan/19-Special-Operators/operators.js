d786be02049ab9ce3842c3baa24ee72fe598bd8f
thebrownbox <conghv123@gmail.com>
2022-03-11 17:07:21 +0700

    UpdatE

--- Phan1_JavaScript_CoBan/19-Special-Operators/operators.js
+++ Phan1_JavaScript_CoBan/19-Special-Operators/operators.js
@@ -40,27 +40,42 @@ function optionalChaining() {
             console.log(this.name + " say Hi!");
         },
     };
-  
+
     console.log(thao.address?.street);
     console.log(cong.address?.street);
     cong.sayHi?.();
     thao.sayHi?.();
 }
 
-function spreadSyntax(){
-    const a = [3,2,1,9];
-    console.log(Math.max(1,2,3));
+function spreadSyntax() {
+    const a = [3, 2, 1, 9];
+    console.log(Math.max(1, 2, 3));
     console.log(Math.max(...a));
-    console.log("x",1,2,3);
+    console.log("x", 1, 2, 3);
 
-    const cong = { 
+    const cong = {
         name: "Cong",
         age: 10,
-        address: "Tran Duy  Hung"
-    }
-    const cuong = {...cong, name: "Cuong"};
+        address: "Tran Duy  Hung",
+    };
+    const cuong = { ...cong, name: "Cuong" };
     console.log(cong);
     console.log(cuong);
 }
 
-spreadSyntax();
+function destructuringAssignment() {
+    const cong = {
+        name: "Cong",
+        age: 10,
+        address: "Tran Duy Hung",
+    };
+
+    const { name, age, abc, ...info } = cong;
+    console.log(name, age, abc, info);
+
+    const arr = [2, 4, 6, 8];
+    const [first, second, ...theRest] = arr;
+    console.log(first, second, theRest);
+}
+
+destructuringAssignment();
