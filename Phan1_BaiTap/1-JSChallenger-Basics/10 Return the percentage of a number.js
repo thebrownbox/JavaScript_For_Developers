2ea23b89ce6272340f7af129f41dc3673269e7b1
thebrownbox <conghv123@gmail.com>
2022-03-20 14:05:23 +0700

    Update

--- Phan1_BaiTap/1-JSChallenger-Basics/10 Return the percentage of a number.js
+++
@@ -1,9 +0,0 @@
-//? Write a function that takes two numbers (a and b) as argument
-//? Return b percent of a
-
-function myFunction(a, b) {
-    return (b / 100) * a;
-}
-
-console.log(myFunction(10, 50)); //* 5
-console.log(myFunction(50, 1)); //* 0.5
