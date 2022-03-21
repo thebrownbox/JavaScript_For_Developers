100366e8744c029a1bd218aae6bc6e964d595195
thebrownbox <conghv123@gmail.com>
2022-03-21 23:29:11 +0700

    update

---
+++ Phan2_JavaScript_OOP/01-Strict-Mode/index.js
@@ -0,0 +1,28 @@
+// "use strict";
+// console.log(this);
+// console.log(undefined);
+// console.log(this.hasOwnProperty(undefined));
+
+// this.undefined = 0;
+// console.log(this.undefined);
+
+// const x = {
+//     get getAge() {
+//         return 10;
+//     },
+// };
+// x.getAge = 100;
+// console.log(x.getAge);
+
+// "use strict";
+
+// function sum(a, a) {
+//     return a + a;
+// }
+
+// console.log(sum(1, 2));
+
+// "use strict";
+// const public = 10;
+
+// console.log(public);
