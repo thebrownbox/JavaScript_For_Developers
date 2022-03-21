ff323f83063fbcdbf433570f85b2b66751594949
thebrownbox <conghv123@gmail.com>
2022-03-22 01:43:46 +0700

    Update

--- Phan2_JavaScript_OOP/03-this-part-1/index.js
+++ Phan2_JavaScript_OOP/03-this-part-1/index.js
@@ -1,2 +1,24 @@
-'use strict';
-a = 10;
+"use strict";
+const GLOBALs = this;
+console.log("this", this);
+
+function f1() {
+    console.log("f1", this);
+}
+const f2 = function () {
+    console.log("f2", this);
+};
+const f3 = () => {
+    console.log("f3", this);
+};
+
+f1();
+f2();
+f3();
+this.f1();
+console.log(this);
+const x = {
+    f() {
+        console.log("xx");
+    },
+};
