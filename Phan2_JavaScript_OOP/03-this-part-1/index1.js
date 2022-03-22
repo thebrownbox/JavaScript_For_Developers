4a5b13eea1e57abd41cacd568106dc5cb40aed4d
Cong Hoang <cong@coderpush.com>
2022-03-22 16:29:14 +0700

    update

--- Phan2_JavaScript_OOP/03-this-part-1/index1.js
+++ Phan2_JavaScript_OOP/03-this-part-1/index1.js
@@ -1,10 +1,23 @@
 "use strict";
 //example 1: global function
 //!Only work with Browser Env
-
-function f1() {
-    console.log("f1", this);
+this.name = "GLOBAL";
+function aFunction() {
+    console.log("aFunction", this?.name);
 }
 
-f1();
-this.f1();
+const x = {
+    name: "x",
+    method() {
+        console.log("method", this?.name);
+    },
+};
+
+aFunction();
+x.method();
+
+x.aFunction = aFunction;
+x.aFunction();
+
+const aMethod = x.method;
+aMethod();
